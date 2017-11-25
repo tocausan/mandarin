d48eaad4aad8caf33800938122c2642cab171866
tocausan <tocausan@gmail.com>
2017-11-25 13:39:52 +0800

    clean

--- services/parser.js
+++ services/parser.js
@@ -1,6 +1,5 @@
 var fs = require('fs');
 
-
 module.exports = {
 
     // read utf8 file
@@ -31,6 +30,7 @@ module.exports = {
             // map lines > object
             lines.map((line) => {
                 if (line.length > 0 && line[0] !== '#') {
+                    // splitted by space, bracket and slash
                     var spaced = line.split(' '),
                         betweenBracket = line.match(/\[(.*)]/),
                         betweenSlashes = line.match(/\/(.*)\//);
@@ -41,17 +41,15 @@ module.exports = {
                         pinyin: betweenBracket ? betweenBracket[0].substr(1, betweenBracket[0].length - 1) : null,
                         translation: betweenSlashes ? betweenSlashes[0].substr(1, betweenSlashes[0].length - 1) : null
                     };
-
                     parsedData.push(parsedLine);
                 }
             });
-
             resolve(JSON.stringify(parsedData));
         });
     },
 
 
-    // chinese hsl cn-en parser
+    // chinese hsk cn-en parser
     chineseHsk: function (data) {
         "use strict";
         return new Promise((resolve, reject) => {
@@ -63,21 +61,20 @@ module.exports = {
             // map lines > object
             lines.map((line) => {
                 if (line.length > 0 && line[0] !== '#') {
-                    var spaced = line.split(' '),
-                        betweenBracket = line.match(/\[(.*)]/),
-                        betweenSlashes = line.match(/\/(.*)\//);
+                    // splitted by tab
+                    var tabed = line.split('\t');
 
+                    console.log(tabed)
                     var parsedLine = {
-                        traditional: spaced ? spaced[0] : null,
-                        simplified: spaced && spaced.length >= 2 ? spaced[1] : null,
-                        pinyin: betweenBracket ? betweenBracket[0].substr(1, betweenBracket[0].length - 1) : null,
-                        translation: betweenSlashes ? betweenSlashes[0].substr(1, betweenSlashes[0].length - 1) : null
+                        simplified: tabed ? tabed[0] : null,
+                        traditional: tabed && tabed.length >= 2 ? tabed[1] : null,
+                        pinyinNumber: tabed && tabed.length >= 3 ? tabed[2] : null,
+                        pinyinTone: tabed && tabed.length >= 4 ? tabed[3] : null,
+                        translation: tabed && tabed.length >= 5 ? tabed[4] : null
                     };
-
                     parsedData.push(parsedLine);
                 }
             });
-
             resolve(JSON.stringify(parsedData));
         })
     }
